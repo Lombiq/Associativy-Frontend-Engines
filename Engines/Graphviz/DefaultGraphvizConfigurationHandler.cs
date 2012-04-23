4eab0d7eeb5eca5d403f79f64f63d339b2403e96
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2012-04-23 23:39:42 +0200

    Producing working urls for nodes that are routable

--- Engines/Graphviz/DefaultGraphvizConfigurationHandler.cs
+++ Engines/Graphviz/DefaultGraphvizConfigurationHandler.cs
@@ -7,17 +7,28 @@ using QuickGraph.Graphviz;
 using Orchard.ContentManagement;
 using Orchard.ContentManagement.Aspects;
 using QuickGraph.Graphviz.Dot;
+using Orchard;
+using System.Web.Mvc;
 
 namespace Associativy.Frontends.Engines.Graphviz
 {
     [OrchardFeature("Associativy.Frontends.Graphviz")]
     public class DefaultGraphvizConfigurationHandler : IGraphvizConfigurationHandler
     {
+        private readonly IOrchardServices _orchardServices;
+
+        public DefaultGraphvizConfigurationHandler(IOrchardServices orchardServices)
+        {
+            _orchardServices = orchardServices;
+        }
+
         public void FormatVertex(object sender, FormatVertexEventArgs<IContent> e)
         {
             // .Has<> doesn't work here
             if (e.Vertex.As<ITitleAspect>() != null) e.VertexFormatter.Label = e.Vertex.As<ITitleAspect>().Title;
-            if (e.Vertex.As<IAliasAspect>() != null) e.VertexFormatter.Url = e.Vertex.As<IAliasAspect>().Path;
+
+            e.VertexFormatter.Url = new UrlHelper(_orchardServices.WorkContext.HttpContext.Request.RequestContext)
+                                        .RouteUrl(_orchardServices.ContentManager.GetItemMetadata(e.Vertex).DisplayRouteValues);
 
             e.VertexFormatter.Shape = GraphvizVertexShape.Diamond;
         }
--- Engines/JIT/DefaultJITConfigurationHandler.cs
+++ Engines/JIT/DefaultJITConfigurationHandler.cs
@@ -7,17 +7,28 @@ using Associativy.Frontends.Engines.JIT.ViewModels;
 using Orchard.ContentManagement.Aspects;
 using Orchard.Environment.Extensions;
 using Associativy.GraphDiscovery;
+using Orchard;
+using System.Web.Mvc;
 
 namespace Associativy.Frontends.Engines.JIT
 {
     [OrchardFeature("Associativy.Frontends.JIT")]
     public class DefaultJITConfigurationHandler : IJITConfigurationHandler
     {
+        private readonly IOrchardServices _orchardServices;
+
+        public DefaultJITConfigurationHandler(IOrchardServices orchardServices)
+        {
+            _orchardServices = orchardServices;
+        }
+
         public void SetupViewModel(IEngineContext engineContext, IGraphContext graphContext, IContent node, NodeViewModel viewModel)
         {
             // .Has<> doesn't work here
             if (node.As<ITitleAspect>() != null) viewModel.name = node.As<ITitleAspect>().Title;
-            if (node.As<IAliasAspect>() != null) viewModel.data["url"] = node.As<IAliasAspect>().Path;
+
+            viewModel.data["url"] = new UrlHelper(_orchardServices.WorkContext.HttpContext.Request.RequestContext)
+                                        .RouteUrl(_orchardServices.ContentManager.GetItemMetadata(node).DisplayRouteValues);
         }
     }
 }
