73388457278096cf062ad7d0e21770e822682f6f
Piedone <piedone@pyrocenter.hu>
2012-08-28 22:56:06 +0200

    Cleaning up Graphviz OpenLayers styles

--- Engines/Graphviz/ResourceManifest.cs
+++ Engines/Graphviz/ResourceManifest.cs
@@ -14,17 +14,16 @@ namespace Associativy.Frontends.Engines.Graphviz
             manifest.DefineScript("AssociativyFrontends_OpenLayers").SetUrl("Engines/Graphviz/OpenLayers-2.11/OpenLayers.js").SetVersion("2.11");
             manifest.DefineScript("AssociativyFrontends_MapQuery").SetUrl("Engines/Graphviz/MapQuery-0.1/src/jquery.mapquery.core.js").SetVersion("0.1").SetDependencies(new string[] { "AssociativyFrontends_OpenLayers", "jQuery" });
             manifest.DefineScript("AssociativyFrontends_MapQuery.ImageLayer").SetUrl("Engines/Graphviz/jquery.mapquery.imageLayer.js").SetDependencies(new string[] { "AssociativyFrontends_MapQuery" });
-            
+            manifest.DefineStyle("AssociativyFrontends_OpenLayers").SetUrl("Engines/Graphviz/associativy-graphviz-openlayers.css");
+
             // Mapz
             manifest.DefineScript("AssociativyFrontends_Mousewheel").SetUrl("Engines/Graphviz/jquery.mousewheel.js").SetDependencies(new string[] { "jQuery" });
             manifest.DefineScript("AssociativyFrontends_Mapz").SetUrl("Engines/Graphviz/jquery.mapz.js").SetDependencies(new string[] { "jQueryUI", "AssociativyFrontends_Mousewheel" });
             manifest.DefineStyle("AssociativyFrontends_Mapz").SetUrl("Engines/Graphviz/associativy-graphviz-mapz.css");
 
             // Mapbox
-            manifest.DefineScript("AssociativyFrontends_Mapz").SetUrl("Engines/Graphviz/jquery.mapbox.js").SetDependencies(new string[] { "jQuery", "AssociativyFrontends_Mousewheel" });
-            manifest.DefineStyle("AssociativyFrontends_Mapz").SetUrl("Engines/Graphviz/associativy-graphviz-mapbox.css");
-
-            //manifest.DefineScript("AssociativyFrontends_Graphviz").SetDependencies(new string[] { "AssociativyFrontends_Mapz" });
+            manifest.DefineScript("AssociativyFrontends_Mapbox").SetUrl("Engines/Graphviz/jquery.mapbox.js").SetDependencies(new string[] { "jQuery", "AssociativyFrontends_Mousewheel" });
+            manifest.DefineStyle("AssociativyFrontends_Mapbox").SetUrl("Engines/Graphviz/associativy-graphviz-mapbox.css");
         }
     }
 }
