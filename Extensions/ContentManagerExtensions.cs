16552b4d41c2b2a4a20c6427a6d6e5e6258cac9f
Zoltán Lehóczky <lehoczky_zoltan@pyrocenter.hu>
2013-04-10 01:06:14 +0200

    Setting the display type of frontend dynamic pages to the graph name
    --HG--
    branch : 1.7

---
+++ Extensions/ContentManagerExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Associativy.GraphDiscovery;
+using Orchard.ContentManagement;
+
+namespace Piedone.HelpfulLibraries.Contents.DynamicPages
+{
+    public static class ContentManagerExtensions
+    {
+        public static dynamic BuildFrontendPageDisplay(this IContentManager contentManager, IContent page, IGraphContext graph)
+        {
+            var shape = contentManager.BuildDisplay(page, graph.Name);
+            shape.Metadata.Wrappers.Add("PageWrapper_" + page.ContentItem.ContentType.Replace('.', '_'));
+            return shape;
+        }
+    }
+}
