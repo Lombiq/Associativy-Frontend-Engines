8ce5e3cead462f7daa1f40210e9c0bd391a1c48b
Piedone <piedone@pyrocenter.hu>
2013-04-11 18:51:50 +0200

    Adding re-ordering by quality of snippet matching to autocomplete list
    --HG--
    branch : 1.7

--- Controllers/AutoCompleteController.cs
+++ Controllers/AutoCompleteController.cs
@@ -36,7 +36,40 @@ namespace Associativy.Frontends.Controllers
 
             if (!IsAuthorized(page)) return new HttpUnauthorizedResult();
 
-            return Json(page.As<IEngineConfigurationAspect>().GraphDescriptor.Services.NodeManager.GetBySimilarLabelQuery(labelSnippet).Slice(15).Select(node => node.As<IAssociativyNodeLabelAspect>().Label), JsonRequestBehavior.AllowGet);
+            var labels = page.As<IEngineConfigurationAspect>().GraphDescriptor.Services.NodeManager.GetBySimilarLabelQuery(labelSnippet).Slice(15).Select(node => node.As<IAssociativyNodeLabelAspect>().Label);
+
+            // Re-ordering labels so the most matching one is at the top. This is necessary since ContentQuery doesn't maintain ordering the of search hits.
+            var trimmedLabelSnippet = labelSnippet.Trim();
+            labels = labels.OrderBy(label => LevenshteinDistance(label, trimmedLabelSnippet));
+
+            return Json(labels, JsonRequestBehavior.AllowGet);
+        }
+
+
+        // Taken from. http://sadgeeksinsnow.blogspot.hu/2010/10/optimizing-levenshtein-algorithm-in-c.html
+        private static int LevenshteinDistance(string s, string t)
+        {
+            int n = s.Length;
+            int m = t.Length;
+            int[,] d = new int[n + 1, m + 1];
+            int cost;
+
+            if (n == 0) return m;
+            if (m == 0) return n;
+
+            for (int i = 0; i <= n; d[i, 0] = i++) ;
+            for (int j = 0; j <= m; d[0, j] = j++) ;
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    cost = (t.Substring(j - 1, 1) == s.Substring(i - 1, 1) ? 0 : 1);
+                    d[i, j] = System.Math.Min(System.Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1), d[i - 1, j - 1] + cost);
+                }
+            }
+
+            return d[n, m];
         }
     }
 }
