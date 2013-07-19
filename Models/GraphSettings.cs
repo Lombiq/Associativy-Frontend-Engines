47dc50533c6cc38391dc467dd5a11fedaa423a1d
Piedone <piedone@pyrocenter.hu>
2013-07-19 23:19:30 +0200

    GraphSettings to its own file
    --HG--
    branch : 1.7

---
+++ Models/GraphSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Associativy.Frontends.Models
+{
+    public class GraphSettings
+    {
+        public int InitialZoomLevel { get; set; }
+        public int ZoomLevelCount { get; set; }
+        public int MaxConnectionCount { get; set; }
+
+        private static GraphSettings _default = new GraphSettings();
+        public static GraphSettings Default { get { return _default; } }
+
+
+        public GraphSettings()
+        {
+            InitialZoomLevel = 0;
+            ZoomLevelCount = 10;
+            MaxConnectionCount = 200;
+        }
+    }
+}
--- Models/IEngineConfigurationAspect.cs
+++ Models/IEngineConfigurationAspect.cs
@@ -12,22 +12,4 @@ namespace Associativy.Frontends.Models
         IMindSettings MindSettings { get; set; }
         GraphSettings GraphSettings { get; set; }
     }
-
-    public class GraphSettings
-    {
-        public int InitialZoomLevel { get; set; }
-        public int ZoomLevelCount { get; set; }
-        public int MaxConnectionCount { get; set; }
-
-        private static GraphSettings _default = new GraphSettings();
-        public static GraphSettings Default { get { return _default; } }
-
-
-        public GraphSettings()
-        {
-            InitialZoomLevel = 0;
-            ZoomLevelCount = 10;
-            MaxConnectionCount = 200;
-        }
-    }
 }
