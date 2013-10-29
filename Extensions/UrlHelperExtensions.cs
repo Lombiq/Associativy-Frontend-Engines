38ec883b3dc17f85a6fa64adb1c534e94e3612f2
Piedone <piedone@pyrocenter.hu>
2013-10-29 13:10:40 +0100

    Frontend engine URL helper is now smarter, contribution of Lombiq (http://lombiq.com/)

--- Extensions/UrlHelperExtensions.cs
+++ Extensions/UrlHelperExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
 using Associativy.Frontends.EngineDiscovery;
 
 namespace Associativy.Frontends.Extensions
@@ -7,9 +9,25 @@ namespace Associativy.Frontends.Extensions
     {
         public static string RouteEngineUrl(this UrlHelper helper, IEngineDescriptor engine, string graphName)
         {
-            var routeValues = ((System.Web.Routing.Route)engine.Route.Route).Defaults;
-            routeValues.Add("GraphName", graphName);
-            return helper.RouteUrl(routeValues);
+            return RouteEngineUrl(helper, engine, graphName, new RouteValueDictionary());
+        }
+
+        public static string RouteEngineUrl(this UrlHelper helper, IEngineDescriptor engine, string graphName, object routeValues)
+        {
+            return RouteEngineUrl(helper, engine, graphName, new RouteValueDictionary(routeValues));
+        }
+
+        public static string RouteEngineUrl(this UrlHelper helper, IEngineDescriptor engine, string graphName, RouteValueDictionary routeValues)
+        {
+            if (routeValues == null) throw new ArgumentNullException("routeValues");
+
+            var originalRouteValues = ((System.Web.Routing.Route)engine.Route.Route).Defaults;
+            originalRouteValues.Add("GraphName", graphName);
+            foreach (var routeValue in routeValues)
+            {
+                originalRouteValues[routeValue.Key] = routeValue.Value;
+            }
+            return helper.RouteUrl(originalRouteValues);
         }
     }
 }
